b6bef98c35f2e2279e0e5184576176643fe59523
Daniel Neamati <danineamati@gmail.com>
2023-04-10 22:23:11 -0700

    Initial Optimizer for project 1

--- project1_jl/project1.jl
+++ project1_jl/project1.jl
@@ -12,7 +12,7 @@
 =#
 
 # Example:
-# using LinearAlgebra
+using LinearAlgebra
 
 #=
     If you're going to include files, please do so up here. Note that they
@@ -41,6 +41,64 @@ Returns:
     - The location of the minimum
 """
 function optimize(f, g, x0, n, prob)
-    x_best = x0
-    return x_best
+    optimize_extra(f, g, x0, n, prob)
+end
+
+
+function optimize_extra(f, g, x0, n, prob, verbose::Bool = false, learning_rate::float = 10^-2)
+    x_curr = x0
+    f_curr = f(x_curr)
+    n = n - 1
+
+    x_tried = [x_curr]
+    f_tried = [f_curr]
+    g_tried = []
+
+    iteration_ind = 1
+    
+    while n > 4
+        # We need 4 evaluations per loop
+
+        # Calculate the Gradient and append it
+        d_gradient = g(x_curr)
+        push!(g_tried, d_gradient)
+        n = n - 2 # Gradients cost 2
+
+        # Calculate the next position based on the learning rate
+        x_new = x_curr - learning_rate * d_gradient
+        f_new = f(x_new)
+        n = n - 1 # Function costs 1
+
+        push!(x_tried, x_new)
+        push!(f_tried, f_new)
+        
+        if verbose println("Iteration $iteration_ind part 1: $x_new with $f_new") end
+
+        # Possible options are:
+        # f_new < f_curr: optimal is somewhere closer to f_new
+        #     ratio > 1 so we should go further
+        # f_curr < f_new: optimal is somewhere closer to f_best
+        #     ratio < 1 so we should stay closer
+        f_ratio = f_curr / f_new
+
+        x_curr = x_curr - learning_rate * d_gradient * f_ratio
+        f_curr = f(x_curr)
+        n = n - 1 # Function costs 1
+
+        push!(x_tried, x_curr)
+        push!(f_tried, f_curr)
+
+        if verbose println("Iteration $iteration_ind part 2: $x_curr with $f_curr (ratio was $f_ratio)") end
+        iteration_ind += 1
+    end
+
+    # Select the point that gave the best f_tried
+    best_ind = argmin(f_tried)
+    x_best = x_tried[best_ind]
+    f_best = f_tried[best_ind]
+
+    if verbose println("Selected $x_best with score $f_best") end
+    if verbose println("$n evaluations left") end
+
+    return x_curr
 end
